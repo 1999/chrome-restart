60d2834445eb8a2863f110164a9e09c834b71d6b
Dmitry Sorin <info@staypositive.ru>
2013-10-10 13:58:07 +0400

    Finally

--- background.js
+++ background.js
@@ -1,3 +1,10 @@
-chrome.browserAction.onClicked.addListener(function () {
-    chrome.tabs.create({url: "chrome://restart", active: false});
-});
+(function () {
+    "use strict";
+
+    function restart() {
+        chrome.tabs.create({url: "chrome://restart", active: false});
+    }
+
+    chrome.browserAction.onClicked.addListener(restart);
+    chrome.commands.onCommand.addListener(restart);
+})();
